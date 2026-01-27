56270b58493a4ae30f1c04079b5fee61e6c94726
anitaparmar26 <adparmar.2010@gmail.com>
2026-01-27 14:55:44 +0530

    fixed the favicon icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,24 +16,13 @@ export const metadata: Metadata = {
  
    icons: {
     icon: [
-      {
-        url: "/favicon-16x16.png",
-        type: "image/png",
-        sizes: "16x16",
-      },
-      {
-        url: "/favicon-dark-32x32.png",
-        type: "image/png",
-        sizes: "32x32",
-      },
-      {
-        url: "/favicon.ico",
-        type: "image/ico",
-      },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: "/apple-touch-icon.png",
   },
-}
+  manifest: "/site.webmanifest",
+};
 
 export const viewport: Viewport = {
   themeColor: "#0f0f0f",
